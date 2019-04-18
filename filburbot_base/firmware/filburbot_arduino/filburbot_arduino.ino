51a5e461d2310443b621305f29cdef52c91efe78
Ricardo Angeli <rangeli93@gmail.com>
2019-04-18 01:09:16 -0400

    Added publishing of Encoders message to filburbot Arduino firmware.

--- filburbot_base/firmware/filburbot_arduino/filburbot_arduino.ino
+++ filburbot_base/firmware/filburbot_arduino/filburbot_arduino.ino
@@ -1,3 +1,7 @@
+#include <ros.h>
+#include <filburbot_msgs/CmdDiffVel.h>
+#include <filburbot_msgs/Encoders.h>
+
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
@@ -6,11 +10,16 @@
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
+ros::NodeHandle_<ArduinoHardware, 1, 1, 128, 512> nh;
+filburbot_msgs::Encoders encoders_msg;
+ros::Publisher pub_encoders("encoders", &encoders_msg);
+
 struct Filburbot_Motor {
   Adafruit_DCMotor * motor;
   int motor_pin;
   int encoder1_pin;
   int encoder2_pin;
+  long position;
   long last_position;
 };
 
@@ -27,6 +36,7 @@ byte Filburbot_Motor_get_encoder_state(Filburbot_Motor *m) {
   return digitalRead(m->encoder2_pin) << 1 | digitalRead(m->encoder1_pin);
 }
 
+Encoder left_enc(4, 5);
 Encoder right_enc(6, 7);
 
 void setup() {
@@ -40,8 +50,8 @@ void setup() {
   right.encoder2_pin = 6;
   Filburbot_Motor_init(&right);
 
-  Serial.begin(9600);
-  Serial.println("Filburbot");
+  // Serial.begin(9600);
+  // Serial.println("Filburbot");
 
   AFMS.begin();
 
@@ -49,55 +59,30 @@ void setup() {
   left.motor->run(BACKWARD);
   right.motor->setSpeed(255);
   right.motor->run(FORWARD);
+
+  // Initialize ROS
+  nh.initNode();
+  nh.advertise(pub_encoders);
 }
-
-long position = -999;
-
-unsigned long initial_time = millis();
 unsigned long last_time = millis();
-unsigned long count = 0;
-bool triggered = false;
+
 
 void loop() {
-  unsigned long now = millis();
-  long current_position = right_enc.read();
-  // if (current_position != position) {
-  //   position = current_position;
-  //   Serial.println(position);
-  // }
+  left.position = left_enc.read();
+  right.position = right_enc.read();
 
-  unsigned long duration = now - last_time;
-  if(duration >= 100) {
-    int speed = (current_position - right.last_position);
-    Serial.println(speed);
-    right.last_position = current_position;
-    last_time = now;
+  if((millis() - last_time) >= 100) {
+    // int speed = (right_position - right.last_position);
+
+    // Serial.println(speed);
+    encoders_msg.left = left.position;
+    encoders_msg.right = right.position;
+    encoders_msg.header.stamp = nh.now();
+    pub_encoders.publish(&encoders_msg);
+
+    // right.last_position = right_position;
+    last_time = millis();
   }
-  // // With any substantial delay added, Encoder can only track
-  // // very slow motion.  You may uncomment this line to see
-  // // how badly a delay affects your encoder.
-  // //delay(50);
 
-  // byte encoder_val = Filburbot_Motor_get_encoder_state(&right);
-  //
-  //
-  // if(encoder_val != last_encoder_val) {
-  //   Serial.println(encoder_val);
-  //   last_encoder_val = encoder_val;
-  // }
-
-  // if(encoder_val == 3) {
-  //   if(!triggered) {
-  //     triggered = true;
-  //     count++;
-  //     if(count > 10) {
-  //       Serial.println(millis() - last_time);
-  //       last_time = millis();
-  //       count = 0;
-  //     }
-  //   }
-  // }
-  // else {
-  //   triggered = false;
-  // }
+  nh.spinOnce();
 }
