7234a7fc1767f68158d05bd3c7b0212d642e0a2a
Ricardo Angeli <rangeli93@gmail.com>
2019-04-19 22:40:02 -0400

    Finished implementing filburbot_base.  Still needs to be tested.

--- filburbot_base/include/filburbot_base/filburbot_hardware.h
+++ filburbot_base/include/filburbot_base/filburbot_hardware.h
@@ -1,9 +1,12 @@
 #ifndef FILBURBOT_BASE_FILBURBOT_HARDWARE_H
 #define FILBURBOT_BASE_FILBURBOT_HARDWARE_H
 
+#include <cmath>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
+#include <filburbot_msgs/Encoders.h>
+#include <filburbot_msgs/CmdDiffVel.h>
 #include <ros/ros.h>
 
 namespace filburbot_base {
@@ -16,11 +19,37 @@ namespace filburbot_base {
     void sendCommandsToMotors();
 
   private:
+    struct Joint {
+      std::string name;
+      double command;
+      double position;
+      double velocity;
+      double effort;
+
+      Joint(const std::string &name): name(name), command(0.0), position(0.0), velocity(0.0), effort(0.0) {}
+    };
+    void encodersCallback(const filburbot_msgs::Encoders &msg);
+
     void registerControlInterfaces();
+
+    inline double toRadians(double encoder_position) {
+      return ((encoder_position / encoder_counts_per_rev_) / gear_reduction_) * 2.0 * M_PI;
+    }
+
+    inline double toEncoderPosition(double radians) {
+      return (radians / 2.0 * M_PI) * gear_reduction_ * encoder_counts_per_rev_;
+    }
 
   private:
     ros::Subscriber sub_encoders_;
     ros::Publisher pub_cmd_diff_;
+
+    Joint left_wheel;
+    Joint right_wheel;
+
+    double encoder_counts_per_rev_;
+    double gear_reduction_;
+    double speed_duration_;
 
     // ROS Control interfaces
     hardware_interface::JointStateInterface joint_state_interface_;
--- filburbot_base/src/filburbot_hardware.cpp
+++ filburbot_base/src/filburbot_hardware.cpp
@@ -2,15 +2,67 @@
 
 namespace filburbot_base {
 
-  FilburbotHardware::FilburbotHardware(ros::NodeHandle nh, ros::NodeHandle private_nh) {
+  FilburbotHardware::FilburbotHardware(ros::NodeHandle nh, ros::NodeHandle private_nh)
+  :
+  left_wheel("left_wheel_joint"),
+  right_wheel("right_wheel_joint"),
+  encoder_counts_per_rev_(44.0),
+  gear_reduction_(40.0),
+  speed_duration_(0.1)
+  {
+    // Retrieve parameters
+    private_nh.param<double>("encoder_counts_per_rev", encoder_counts_per_rev_, encoder_counts_per_rev_);
+    private_nh.param<double>("gear_reduction", gear_reduction_, gear_reduction_);
+    private_nh.param<double>("speed_duration", speed_duration_, speed_duration_);
+
+    // Register publishers and subscribers
+    pub_cmd_diff_ = nh.advertise<filburbot_msgs::CmdDiffVel>("/cmd_diff", 10);
+    sub_encoders_ = nh.subscribe("/encoders", 10, &FilburbotHardware::encodersCallback, this);
+
+    registerControlInterfaces();
 
   }
 
   void FilburbotHardware::sendCommandsToMotors() {
-
+    filburbot_msgs::CmdDiffVel msg;
+    msg.left_speed = static_cast<int>(toEncoderPosition(left_wheel.command) * speed_duration_);
+    msg.right_speed = static_cast<int>(toEncoderPosition(right_wheel.command) * speed_duration_);
+    pub_cmd_diff_.publish(msg);
   }
 
   void FilburbotHardware::registerControlInterfaces() {
+    // Connect and register the joint state interface
+    hardware_interface::JointStateHandle left_wheel_joint_state_handle(
+        left_wheel.name,
+        &left_wheel.position,
+        &left_wheel.velocity,
+        &left_wheel.effort);
+    joint_state_interface_.registerHandle(left_wheel_joint_state_handle);
 
+    hardware_interface::JointStateHandle right_wheel_joint_state_handle(
+        right_wheel.name,
+        &right_wheel.position,
+        &right_wheel.velocity,
+        &right_wheel.effort);
+    joint_state_interface_.registerHandle(right_wheel_joint_state_handle);
+
+    registerInterface(&joint_state_interface_);
+
+    // Connect and register the joint velocity interface
+    hardware_interface::JointHandle left_wheel_joint_handle(left_wheel_joint_state_handle, &left_wheel.command);
+    velocity_joint_interface_.registerHandle(left_wheel_joint_handle);
+
+    hardware_interface::JointHandle right_wheel_joint_handle(right_wheel_joint_state_handle, &right_wheel.command);
+    velocity_joint_interface_.registerHandle(right_wheel_joint_handle);
+
+    registerInterface(&velocity_joint_interface_);
+  }
+
+  void FilburbotHardware::encodersCallback(const filburbot_msgs::Encoders &msg) {
+    left_wheel.position = toRadians(msg.left_position);
+    left_wheel.velocity = toRadians(msg.left_speed) / speed_duration_;
+
+    right_wheel.position = toRadians(msg.right_position);
+    right_wheel.velocity = toRadians(msg.right_speed) / speed_duration_;
   }
 }
